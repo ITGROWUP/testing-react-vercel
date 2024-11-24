083b2aa12aae6c850c279f967ceb549a1957d90e
anandava <anandava759@gmail.com>
2024-11-24 15:18:15 +0700

    footer responsif

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Home/>
       <Product/>
       <ProdukKami />
-      {/* <Footer/> */}
+      <Footer/>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,60 +1,69 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-10'>
-        <div className='w-[100%] h-[250px] bg-[#4800DD] flex justify-center gap-40 pt-6 text-gray-200'>
-            <div className='flex flex-col'>
-                <div className='text-xl mb-5'>ANTORMA</div>
-                <div className='text-lg font-nexa-light mb-1'>Pusat Bantuan</div>
-                <div className='text-lg font-nexa-light mb-1'>Cek Orderan</div>
-                <div className='text-lg font-nexa-light mb-1'>Hubungi Kami</div>
-                <div className='text-lg font-nexa-light mb-1'>Lainnya</div>
-            </div>
-            <div className='flex flex-col'>
-                <div className='text-xl mb-5'>Tentang Perusahaan</div>
-                <div className='text-lg font-nexa-light mb-1'>Work With Us</div>
-                <div className='text-lg font-nexa-light mb-1'>Visi & Misi</div>
-                <div className='text-lg font-nexa-light mb-1'>antorma.com</div>
-            </div>
-            <div className='flex flex-col'>
-                <div className='text-xl mb-5'>Discover</div>
-                <div className='text-lg font-nexa-light mb-1'>Jadi Mitra</div>
-                <div className='text-lg font-nexa-light mb-1'>Catalog</div>
-                <div className='text-lg font-nexa-light mb-1'>Tag Furniture</div>
-            </div>
+    <div className="flex flex-col justify-center items-center gap-10">
+      {/* Bagian Utama Footer */}
+      <div className="w-full h-auto bg-[#4800DD] flex flex-wrap justify-center sm:justify-center  gap-10 sm:gap-40 p-6 text-gray-200">
+        {/* Kolom 1 */}
+        <div className="flex flex-col text-center sm:text-left">
+          <div className="text-xl mb-5">ANTORMA</div>
+          <div className="text-lg font-nexa-light mb-1">Pusat Bantuan</div>
+          <div className="text-lg font-nexa-light mb-1">Cek Orderan</div>
+          <div className="text-lg font-nexa-light mb-1">Hubungi Kami</div>
+          <div className="text-lg font-nexa-light mb-1">Lainnya</div>
         </div>
-        <div className='flex flex-col gap-6'>
-            <div className='text-center text-xl font-light'>Ikuti Kami</div>
-            <div className='flex gap-4 items-center justify-center'>
-                <img className='w-10' src="/images/Facebook.png" alt="facebook" />
-                <img className='w-10' src="/images/Facebook.png" alt="facebook" />
-            </div>
-            <div>
-                <div className='font-nexa-light'>
-                    Copyright 2024 Antorma Interior Solution. All right reserved.
-                </div>
-                <div className='text-center'>
-                <a href="#" className='font-nexa-light underline justify-center'>antorma.com</a>
-                </div>
-            </div>
+        {/* Kolom 2 */}
+        <div className="flex flex-col text-center sm:text-left">
+          <div className="text-xl mb-5">Tentang Perusahaan</div>
+          <div className="text-lg font-nexa-light mb-1">Work With Us</div>
+          <div className="text-lg font-nexa-light mb-1">Visi & Misi</div>
+          <div className="text-lg font-nexa-light mb-1">antorma.com</div>
         </div>
-        <div className='flex gap-8'>
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
-            <img className='w-8' src="/images/Facebook.png" alt="facebook" />
+        {/* Kolom 3 */}
+        <div className="flex flex-col text-center sm:text-left">
+          <div className="text-xl mb-5">Discover</div>
+          <div className="text-lg font-nexa-light mb-1">Jadi Mitra</div>
+          <div className="text-lg font-nexa-light mb-1">Catalog</div>
+          <div className="text-lg font-nexa-light mb-1">Tag Furniture</div>
         </div>
-    </div>
-    )
-}
+      </div>
 
-export default Footer
+      {/* Ikuti Kami */}
+      <div className="flex flex-col gap-6 w-full items-center text-center px-4">
+        <div className="text-xl font-light">Ikuti Kami</div>
+        <div className="flex gap-4 items-center justify-center">
+          <img className="w-10" src="/images/Facebook.png" alt="facebook" />
+          <img className="w-10" src="/images/Facebook.png" alt="facebook" />
+        </div>
+        <div className="text-sm sm:text-base font-nexa-light">
+          <div>
+            Copyright 2024 Antorma Interior Solution. All right reserved.
+          </div>
+          <div>
+            <a
+              href="#"
+              className="font-nexa-light underline block text-center"
+            >
+              antorma.com
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Ikon Media Sosial */}
+      <div className="flex flex-wrap gap-4 justify-center items-center">
+        {[...Array(12)].map((_, i) => (
+          <img
+            key={i}
+            className="w-8"
+            src="/images/Facebook.png"
+            alt={`icon-${i}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
